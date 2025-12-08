475c4d85a07e6efcce1f7959ba78d53f654c56d9
saviinda <savi077nda@gmail.com>
2025-12-08 16:37:57 +0530

    initial commit

---
+++ src/bean/SpringBean.java
@@ -0,0 +1,11 @@
+package bean;
+
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class SpringBean {
+    public SpringBean() {
+        System.out.println("Spring Bean Object Created ");
+    }
+}
